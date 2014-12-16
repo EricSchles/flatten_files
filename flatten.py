d466d63b5c2543094bd9537f4574a2050f8ac395
EricSchles <ericschles@gmail.com>
2014-12-16 07:18:24 -0800

    works

--- flatten.py
+++ flatten.py
@@ -1,19 +1,21 @@
 import os
 import shutil
 import argparse
+from glob import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument("root")
 args = parser.parse_args()
 
-root = args.root[0]
-root_dir = os.path.abspath(root)
+root = args.root
 
-def move_files(root,root_dir):
-    for rootdir,dir,files in os.walk(root):
-        for file in files:
-            filename = file
-            file = os.path.join(rootdir,file)
-            shutil.move(file,root_dir+filename)
+
+for rootdir,dirs,files in os.walk(root):
+    for name in files:
+        filename = name
+        name = os.path.join(rootdir,name)
+        shutil.move(name,root+filename)
     
-
+for rootdir,dirs,files in os.walk(root):
+    for name in dirs:
+        os.rmdir(os.path.join(root,name))
